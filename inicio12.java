92654ca886431859d62339a2b59553eebce06e87
José Saavedra <josesaavedra@MacBook-Pro.local>
2020-06-08 16:21:00 -0500

    add documentation to code

--- inicio12.java
+++ inicio12.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class inicio12 {
     public static void main(String[] args) {
     	
-    	 int n=0, i, max=0, pos=0, maxp=0, posp=0;
-    	 String x;
+    	 int n=0, i, max=0, maxp=0;
     	 String nomE[] = new String[10];
     	 String simb[] = new String[10];
     	 int nAtomico[] = new int[10];  	
@@ -19,21 +18,21 @@ public class inicio12 {
   				System.out.println("Fernando Estribi | 8-969-1399 | 1SF121");
   				System.out.println("Sebastian Arrivillaga | E-8-159257 | 1SF121");
   				System.out.println(" ");
-  	// --------------------------------------------------------------
+  	// ----------------------------------------------------------------
   				
   				System.out.println("Cuantos elementos desea insertar?: ");
   				try (Scanner lee = new Scanner(System.in)) {
 					n=lee.nextInt();
 					System.out.println(" ");
 					
-					
+					//lectura de los elementos con sus respectivos datos
 					for(i=1 ; i <= n; i++)
 					{
 						System.out.println("Inserte el numero atomico #"+i);
 						nAtomico[i]=lee.nextInt();
 						System.out.println("Inserte el peso atomico #"+i);
 						pesoA[i]=lee.nextInt();
-						x=lee.nextLine();
+						lee.nextLine();
 						System.out.println("Inserte el nombre del elemento #"+i);
 						nomE[i]=lee.nextLine();
 						System.out.println("Inserte el simbolo del elemento #"+i);
@@ -47,11 +46,11 @@ public class inicio12 {
 						
 						if(nAtomico[i]>max) {
 							max=nAtomico[i];
-							pos=i;
+							
 						}
 						if(pesoA[i]>maxp) {
 							maxp=nAtomico[i];
-							posp=i;
+							
 						}
 					}
 					
